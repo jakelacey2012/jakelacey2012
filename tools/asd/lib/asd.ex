aebce28154ec42e878ca5fe1b419d52112a59981
Jake Lacey <jakewlacey@gmail.com>
2020-09-22 15:10:45 +0100

    adding ping command

--- tools/asd/lib/asd.ex
+++ tools/asd/lib/asd.ex
@@ -5,24 +5,30 @@ defmodule ASD do
 
   def main(argv) do
     {options, command, _} = OptionParser.parse(argv,
-      switches: [question: :string],
-      aliases: [q: :question]
+      switches: [url: :string],
+      aliases: [u: :url]
     )
 
-    try do
-      ASD.call(command, options)
-    rescue
-      e in RuntimeError -> e
-        IO.puts e.message
-    end
+    ASD.call(command, options)
   end
 
   def call(["hello"], _options) do
     IO.puts("hello to you too!")
   end
 
+  def call(["ping"], options) do
+    url = options[:url]
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200}} ->
+        IO.puts "200"
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, _} ->
+        IO.puts "Error"
+    end
+  end
+
   def call(_, _) do
     IO.puts("invalid arguments")
   end
-
 end
